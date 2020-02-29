7e2fbd5543084b18d91621ff779ea47d67a8a705
Curtice DeSota <curtice.desota@gmail.com>
2020-02-28 19:32:28 -0500

    adding theming and buttons

---
+++ main.js
@@ -0,0 +1,20 @@
+const secret = document.querySelector('.secret');
+const theme = document.querySelector('.theme');
+
+function changeTheme() {
+    const container = document.querySelector('.container');
+    container.style.backgroundColor = '#333';
+
+    const text = document.querySelectorAll('.container p');
+    text[0].style.color = 'white';
+    text[1].style.color = 'white';
+    text[2].style.color = 'white';
+
+    const projects = document.querySelector('.projects');
+    projects.style.backgroundColor = '#333';
+
+    const projectHead = document.querySelector('#project-head');
+    projectHead.style.color = 'white';
+}
+
+theme.onclick = changeTheme;
