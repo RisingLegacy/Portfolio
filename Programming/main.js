58e79cdba502601f29710b21dc17fe8187df0e86
Curtice DeSota <37026059+RisingLegacy@users.noreply.github.com>
2020-03-04 08:46:45 -0500

    fixed formatting

--- Programming/main.js
+++ Programming/main.js
@@ -1,27 +1,27 @@
-const secret = document.querySelector('.secret');
-const theme = document.querySelector('.theme');
+const secret = document.querySelector(".secret");
+const theme = document.querySelector(".theme");
 
 function changeTheme() {
-    const container = document.querySelector('.container');
-    container.style.backgroundColor = '#333';
+  const container = document.querySelector(".container");
+  container.style.backgroundColor = "#333";
 
-    const text = document.querySelectorAll('.container p');
-    text[0].style.color = 'white';
-    text[1].style.color = 'white';
-    text[2].style.color = 'white';
+  const text = document.querySelectorAll(".container p");
+  text[0].style.color = "white";
+  text[1].style.color = "white";
+  text[2].style.color = "white";
 
-    const projects = document.querySelector('.projects');
-    projects.style.backgroundColor = '#333';
+  const projects = document.querySelector(".projects");
+  projects.style.backgroundColor = "#333";
 
-    const projectHead = document.querySelector('#project-head');
-    projectHead.style.color = 'white'; 
+  const projectHead = document.querySelector("#project-head");
+  projectHead.style.color = "white";
 }
 
 theme.onclick = changeTheme;
 
 function nyanCats() {
-    const overlay = document.querySelector('#overlay');
-    overlay.style.display = 'block';
+  const overlay = document.querySelector("#overlay");
+  overlay.style.display = "block";
 }
 
 secret.onclick = nyanCats;
